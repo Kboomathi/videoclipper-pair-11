d0d50dd2f1cd97d6013183e6263c8530d1f279f2
ss18abv <ss18abv@ENT-D410-50.herts.ac.uk>
2019-04-08 13:57:15 +0100

    testSetTitleToEmptyStringKeepsPreviousValue

--- test/embeddedmediaplayer/ClipTest.java
+++ test/embeddedmediaplayer/ClipTest.java
@@ -57,20 +57,7 @@ public class ClipTest {
     @Test
     public void testSetEndBeforeStartKeepsPreviousValue()
     {
-        System.out.println("Set Time");
-         Clip instance = new Clip();
-       String OriginalTitle = "Sub video"; //Create sub video clip
-       instance.setTitle(OriginalTitle);   // Set Ttile  
-       int OriginalStartTime = 10;
-       int originalEndtime = 50;
-       instance.setMax(100);//set master video to 100 seconds video
-       instance.setStart(OriginalStartTime); 
-       instance.setEnd(originalEndtime);
-       int EndTime = instance.getEnd(); //Get endtime of the video
-       instance.setStart(EndTime); //Set end time as start time to the sub clip
-       int CurrentStartTime = instance.getStart();//check time has changed
-       assertEquals(OriginalStartTime,CurrentStartTime);
-       System.out.println("Time has been set");
+        
     
     }
 
@@ -78,72 +65,18 @@ public class ClipTest {
     public void testEqualsOnEqualClips() 
     {
         
-       System.out.println("Check clips are equal");
-       
-       //Create first sub video clip
-       String SubTitle1 = "Sub video 1"; 
-       Clip subClip1 = new Clip(SubTitle1,5,40);
-       
-       //Create second sub video clip
-       String SubTitle2 = "Sub video 1"; 
-       Clip subClip2 = new Clip();
-        
-       subClip2.setTitle(SubTitle2);   // Set Ttile  
-       subClip2.setStart(5);//Set start time 
-       subClip2.setEnd(40);// set end time 
-       
-       // Check for Duplication
-       
-       boolean DuplicateClip = subClip1.equals(subClip2); 
-       assertEquals(true,DuplicateClip);
-       System.out.println("Duplicate video"); 
-       
+   
     }
     
     @Test
     public void testEqualsOnNonEqualClips() 
     {
-             
-       System.out.println("Non Equal Clips");
-       
-       //Create first sub video clip
-       
-       String SubTitle1 = "Sub video 1"; 
-       Clip subClip1 = new Clip(SubTitle1,5,40);
-       
-       //Create second sub video clip
-       
-       String SubTitle2 = "Sub video 2"; 
-       Clip subClip2 = new Clip();
-        
-       subClip2.setTitle(SubTitle2);   // Set Ttile  
-       subClip2.setStart(10);//Set start time 
-       subClip2.setEnd(60);// set end time 
-       
-       // Check for Duplication
-       
-       boolean DuplicateClip = subClip1.NotEquals(subClip2); 
-       assertEquals(true,DuplicateClip);
-       System.out.println("Non Duplicate video"); 
+      
     }
     
     @Test
     public void testSetEndToNegativeNumberKeepsPreviousValue() 
     {
-         System.out.println("Set Time");
-         Clip instance = new Clip();
-       String OriginalTitle = "Sub video"; //Create sub video clip
-       instance.setTitle(OriginalTitle);   // Set Ttile  
-       int OriginalStartTime = 10;
-       int originalEndtime = 50;
-       instance.setMax(100);//set master video to 100 seconds video
-       instance.setStart(OriginalStartTime); 
-       instance.setEnd(originalEndtime);
-       int EndTime = instance.getEnd(); //Get endtime of the video
-       instance.setStart(EndTime); //Set end time as start time to the sub clip
-       int CurrentStartTime = instance.getStart();//check time has changed
-       assertEquals(OriginalStartTime,CurrentStartTime);
-       System.out.println("Time has been set");
         
     }
     
